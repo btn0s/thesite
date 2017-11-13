5d92a5d266b115e69d69eaf0eb932e9a89b7758f
Brendan Norris <brendan.t.norris@gmail.com>
2017-11-13 06:56:34 +0000

    finally working

--- gulpfile.js
+++ gulpfile.js
@@ -2,29 +2,40 @@ var gulp = require('gulp');
 var flatten = require('gulp-flatten');
 var clean = require('gulp-clean');
 var sequence = require('gulp-sequence');
+var sass = require('gulp-sass');
 
-gulp.task('clean', function () {
-  return gulp.src('./app/build/**/*')
+gulp.task('clean', function (done) {
+  return gulp.src('./app/build/')
   .pipe(clean());
+  done();
 });
 
-gulp.task('html', function () {
+gulp.task('html', function (done) {
   gulp.src('./app/src/**/*.html')
   .pipe(flatten())
   .pipe(gulp.dest('./app/build/'));
+  done();
 });
 
-gulp.task('js', function(){
+gulp.task('js', function(done){
   gulp.src('./app/src/assets/js/app.js')
   // .pipe(flatten())
   .pipe(gulp.dest('./app/build/js'));
+  done();
 });
 
-gulp.task('scss', function(){
+gulp.task('scss', function(done){
   gulp.src('./app/src/assets/scss/app.scss')
-  // .pipe(flatten())
+  .pipe(sass().on('error', sass.logError))
   .pipe(gulp.dest('./app/build/css'));
+  done();
 });
 
-gulp.task('build', ['html','js','scss']);
-gulp.task('default', sequence('clean','build'));
+gulp.task('watch', function(){
+  gulp.watch('./app/src/**/*.html').on('all', gulp.series('html'));
+  gulp.watch('./app/src/**/*.scss').on('all', gulp.series('scss'));
+  gulp.watch('./app/src/**/*.js').on('all', gulp.series('js'));
+});
+
+gulp.task('build', gulp.parallel('html','js','scss'));
+gulp.task('default', gulp.series('clean','build','watch'));
